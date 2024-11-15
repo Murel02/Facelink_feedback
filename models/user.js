23d41b9a30c44323268594cba7f04c4402648c8d
Murel02 <olimurel@gmail.com>
2024-11-15 10:54:36 +0100

    Changes by mikke

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
-// Model file 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: {type: String, required: true},
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+});
+
+module.exports = mongoose.model('User', userSchema);
